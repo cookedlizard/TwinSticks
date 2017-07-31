6f21796374ceacf0a3d8c56e46a6874388d33025
Andrew Bennett <cookedlizard@gmail.com>
2017-07-30 17:05:36 -0700

    End of Course

---
+++ Assets/GameManager/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class GameManager : MonoBehaviour {
+
+
+	public bool recording = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print(CrossPlatformInputManager.GetButton("Fire1"));
+		if (CrossPlatformInputManager.GetButton ("Fire1")) {
+			recording = false;
+
+		} else
+			recording = true;	
+		print (recording);
+	}
+
+	public void setRecording(){
+		recording = true;
+	}
+	public void setPlayBack(){
+		recording = false;
+	}
+
+}
---
+++ Assets/Player/ReplaySystem.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ReplaySystem	 : MonoBehaviour {
+
+	private const int bufferFrames = 100;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame [bufferFrames];
+	private Rigidbody rb;
+	private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		gameManager = GameObject.FindObjectOfType<GameManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (!gameManager.recording) {
+			PlayBack ();
+		} else
+			Record ();
+	}
+	void PlayBack(){
+		Debug.Log ("Entered PlayBack");
+		rb.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		//print ("Reading frame " + frame);
+		transform.position = keyFrames [frame].position;
+		transform.rotation = keyFrames [frame].rotation;
+	}
+	void Record ()
+	{
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		//print ("Write Frame " + frame);
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+	}
+}
+/// <summary>
+/// A Structure for storing time, position, and rotation.
+/// </summary>
+public struct MyKeyFrame{
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+	public MyKeyFrame(float time, Vector3 pos, Quaternion rot){
+		frameTime = time;
+		position = pos;
+		rotation = rot;
+
+	}
+}
